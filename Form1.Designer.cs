ad11aeac67aeae7f67657ecf3dce3699348bc7d1
TamNhien <canhthien91@gmail.com>
2024-06-10 13:52:45 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CS_BangCuuChuong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.txtSo = new System.Windows.Forms.TextBox();
+            this.lsKQ = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BẢNG CỬU CHƯƠNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(372, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Bạn muốn hiển thị cửu chương mấy ?";
+            // 
+            // btnXem
+            // 
+            this.btnXem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.Location = new System.Drawing.Point(423, 102);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(75, 36);
+            this.btnXem.TabIndex = 1;
+            this.btnXem.Text = "Xem";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // txtSo
+            // 
+            this.txtSo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSo.Location = new System.Drawing.Point(65, 113);
+            this.txtSo.Name = "txtSo";
+            this.txtSo.Size = new System.Drawing.Size(179, 30);
+            this.txtSo.TabIndex = 2;
+            // 
+            // lsKQ
+            // 
+            this.lsKQ.FormattingEnabled = true;
+            this.lsKQ.Location = new System.Drawing.Point(65, 173);
+            this.lsKQ.Name = "lsKQ";
+            this.lsKQ.Size = new System.Drawing.Size(432, 225);
+            this.lsKQ.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(563, 450);
+            this.Controls.Add(this.lsKQ);
+            this.Controls.Add(this.txtSo);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.TextBox txtSo;
+        private System.Windows.Forms.ListBox lsKQ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_BangCuuChuong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            lsKQ.Items.Clear();
+            int cc = Convert.ToInt32(txtSo.Text);
+            for (int i = 1; i <= 10; i++)
+            {
+                //lsKQ.Items.Add(txtSo.Text + " x " + i.ToString() + " = " + Convert.ToString(cc*i));
+                lsKQ.Items.Add(string.Format("{1} x {0} = {2}", txtSo.Text, i.ToString(), Convert.ToString(cc*i)));
+            }    
+        }
+    }
+}
